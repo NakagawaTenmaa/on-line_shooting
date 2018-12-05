da99a7b6bade557020e48776d1b707e9bbe168f9
nakagawatenma <tenko1177@gmail.com>
2018-12-05 20:07:55 +0900

    Animation基本関数の 追加

--- shooting/Animation/Animation.cpp
+++ shooting/Animation/Animation.cpp
@@ -1,11 +1,24 @@
 ﻿#include "Animation.h"
-
+#include "../Common/DrawManager.h"
 
 /// <summary>
 /// コンストラクタ
 /// </summary>
-Animation::Animation()
+Animation::Animation(){}
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+/// <param name="_row">     行</param>
+/// <param name="_column">  列</param>
+/// <param name="_fileName">ファイル名</param>
+Animation::Animation(int _row, int _column, int _size, RECT _rect, wchar_t *_fileName)
 {
+	Draw::DrawManager::GetInstance()->LoadTexture(m_image, _fileName);
+	m_row = _row;
+	m_column = _column;
+	m_size = _size;
+	m_rect = _rect;
 }
 
 /// <summary>
@@ -13,4 +26,20 @@ Animation::Animation()
 /// </summary>
 Animation::~Animation()
 {
+}
+
+/// <summary>
+/// 更新
+/// </summary>
+void Animation::Update()
+{
+
+}
+
+/// <summary>
+/// 描画
+/// </summary>
+void Animation::Render()
+{
+	Draw::DrawManager::GetInstance()->Render(m_image);
 }
--- shooting/Animation/Animation.h
+++ shooting/Animation/Animation.h
@@ -1,13 +1,28 @@
 ﻿#pragma once
+#include "../Common/DrawData.h"
 
 class Animation
 {
 public:
 	// コンストラクタ
 	Animation();
+	Animation(int _row, int _column, int _size, RECT _rect, wchar_t *_fileName);
 	// デストラクタ
 	~Animation();
 
+	// 更新
+	void Update();
+	// 描画
+	void Render();
 private:
-
+	// 画像
+	Draw::DrawData m_image;
+	// 行
+	int m_row;
+	// 列
+	int m_column;
+	// 画像
+	int m_size;
+	// 画像サイズ
+	RECT m_rect;
 };
