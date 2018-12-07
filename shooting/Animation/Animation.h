b3e766edf5f40db74f98053943ce13fa4374d6ee
nakagawatenma <tenko1177@gmail.com>
2018-12-07 15:41:22 +0900

    Animation再投稿

---
+++ shooting/Animation/Animation.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "../Common/DrawData.h"
+
+class Animation
+{
+public:
+	// コンストラクタ
+	Animation();
+	Animation(int _size, RECT _rect, wchar_t *_fileName, int _time = 1);
+	// デストラクタ
+	~Animation();
+
+	// 更新
+	void Update();
+	// 描画
+	void Render(DirectX::SimpleMath::Vector2 _pos);
+
+private:
+	// 時間計測
+	int Timer();
+
+private:
+	// 画像
+	Draw::DrawData m_image;
+
+	// 画像
+	int m_size;
+	// 画像サイズ
+	RECT m_rect;
+	// 切り替え時間
+	int m_time;
+	// 経過時間
+	int m_timer;
+	// 何枚目
+	int m_count;
+	// 時間計測用
+	int m_timeCount;
+};
