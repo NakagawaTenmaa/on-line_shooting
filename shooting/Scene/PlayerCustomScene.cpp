ce81fdc1a7af97273d01fa39b5b4c2c9d16a0f97
nakagawatenma <tenko1177@gmail.com>
2018-12-11 10:10:12 +0900

    プレイヤー強化シーンの追加

---
+++ shooting/Scene/PlayerCustomScene.cpp
@@ -0,0 +1,33 @@
+﻿#include "PlayerCustomScene.h"
+
+using namespace Scene;
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+PlayerCustomScene::PlayerCustomScene()
+{
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+PlayerCustomScene::~PlayerCustomScene()
+{
+}
+
+/// <summary>
+/// 更新
+/// </summary>
+/// <returns></returns>
+bool PlayerCustomScene::Update()
+{
+	return true;
+}
+
+/// <summary>
+/// 描画
+/// </summary>
+void PlayerCustomScene::Render()
+{
+}
---
+++ shooting/Scene/PlayerCustomScene.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "SceneBase.h"
+
+namespace Scene
+{
+	/// <summary>
+	/// プレイヤーの強化シーン
+	/// </summary>
+	class PlayerCustomScene : public SceneBase
+	{
+	public:
+		// コンストラクタ
+		PlayerCustomScene();
+		// デストラクタ
+		~PlayerCustomScene();
+
+		// 描画
+		bool Update() override;
+		// 更新
+		void Render() override;
+	private:
+
+	};
+};
